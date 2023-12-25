c82171951ba4188a1d7229e73aee9e186986c6b5
fsjhut <1067224906@qq.com>
2023-12-25 14:48:19 +0800

    更新：后端更新

--- apps/chatbot/output/consumers.py
+++ apps/chatbot/output/consumers.py
@@ -3,6 +3,9 @@ import logging
 from channels.generic.websocket import WebsocketConsumer
 from asgiref.sync import async_to_sync
 
+from apps.chatbot.output.realtime_message_queue import RealtimeMessageQueryJobTask
+
+# chat_channel = "chat"
 chat_channel = "chat_channel"
 logger = logging.getLogger(__name__)
 
@@ -10,13 +13,24 @@ logger = logging.getLogger(__name__)
 class ChatConsumer(WebsocketConsumer):
 
     def connect(self):
+        # 获取 WebSocket 连接的房间名
+        user_id = self.scope["path_remaining"][1:]
+
+        # self.room_group_name = f"chat_{room_name}"
         # 接受 WebSocket 连接
         self.accept()
+
         # 将连接的客户端添加到特定频道
+        # async_to_sync(self.channel_layer.group_add)(
+        #     f"{chat_channel}_{user_id}",  # 设置频道名称
+        #     self.channel_name
+        # )
         async_to_sync(self.channel_layer.group_add)(
             chat_channel,  # 设置频道名称
             self.channel_name
         )
+        logger.info(f'=> ws self : {user_id}，，，，')
+        # RealtimeMessageQueryJobTask.start(user_id)
         logger.info(f'=> ws connect group : {chat_channel}')
 
     # 从特定频道中移除连接的客户端。
@@ -33,7 +47,7 @@ class ChatConsumer(WebsocketConsumer):
         logger.info(f"=> run receive:{text_data}")
         # self.send(text_data=json.dumps({"message": text_data}))
 
-      # Receive message from room group
+    # Receive message from room group
     def chat_message(self, event):
         message = event["message"]
         logger.info(f"=> run chat_message :{message}")
